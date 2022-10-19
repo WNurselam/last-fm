0f0759533aa6effa9aa6003b10f7456a151f5845
Nurselam <77880259+WNurselam@users.noreply.github.com>
2022-10-20 01:22:07 +0300

    update

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -9,10 +9,10 @@ const Track = ({ track }) => {
         <strong>{track.name}</strong>
         <p>{track.artist.name}</p>
       </div>
-      <div>
+      <span>
         <p><i className="fa-solid fa-headphones"></i>:{track.listeners}</p>
         <p><i className="fa-sharp fa-solid fa-circle-play"></i>:{track.playcount}</p>
-      </div>
+      </span>
     </div>
   );
 };
